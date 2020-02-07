2d2b00a7feb30336dac63d0d7186ba8132bb475a
Robert Khan <hanafievrobert@mail.ru>
2020-02-07 12:10:45 +0300

    Signal list now is forming directly from log file

--- buildGraph.py
+++ buildGraph.py
@@ -5,29 +5,21 @@ import matplotlib.backend_bases as bkndbs
 import datetime as time
 from zoomPan import*
 
-
 # Constants and other values
-global THRESHOLD_OVER_VOLTAGE, THRESHOLD_UNDER_VOLTAGE, THRESHOLD_CHARGE_VOLTAGE
 THRESHOLD_OVER_VOLTAGE = 3700
 THRESHOLD_UNDER_VOLTAGE = 2000
 THRESHOLD_CHARGE_VOLTAGE = 21600
 THRESHOLD_DISCHARGE_VOLTAGE = 12000
+SIGNALS_NUMBER = 66                             # number of data columns
+SIGNAL_LINE_IN_LOGFILE = 15                     # signal names in log file can be found in this line
+DATA_START_IN_LOGFILE = 16                      # data in log file starts from string with this number
+XAXIS_RAW_IN_LOGFILE = "DateTime"               # have to be in "smth HH:MM:SS" format. Otherwise 'CalculateXaxis()' func have to be modified
 
-SIGNALS_NUMBER = 66             # number of data columns
-DATA_START_IN_LOGFILE = 16      # data in log file starts from string with this number
-global DATA_LIST, SIGNAL_LIST
-DATA_LIST = []
-SIGNAL_LIST = ["Sample", "DateTime", "ElapsedTime", "ManufAccess", "RemCapAlarm", "RemTimeAlarm", "BattMode", "@Rate(@)", "@TimeFull",
-               "@TimeEmpty", "@RateOK", "Temperature", "Voltage", "Current", "AvgCurr", "MaxErr", "RSOC", "ASOC", "RemCap", "FullChgCap",
-               "RunTimeEmty", "RunTimeEmty", "AvgTimeFull", "ChgCurr", "ChgVolt", "BattStat", "CycleCnt", "PendingEdv", "SoH", "OpStatA",
-               "OpStatB", "TempRange", "ChgStat", "GaugeStat", "MfgStat", "SafetyAlertAB", "SafetyStatAB", "SafetyAlertCD", "SafetyStatCD",
-               "PFAlertAB", "PFStatAB", "PFStatAB", "CellVolt1", "CellVolt2", "CellVolt3", "CellVolt4", "CellVolt5", "CellVolt6", "CellVolt7",
-               "CellVolt8", "CellVolt9", "CellVolt10", "CellVolt11", "CellVolt12", "CellVolt13", "CellVolt14", "CellVolt15", "ExtAvgCellVolt",
-               "TS1 Temp", "TS2 Temp", "TS3 Temp", "CellTemp", "FetTemp", "GaugeInternalTemp", "LogRowTime(ms)", "LogStatus"]
 
 class LogGraph:
     def __init__(self):
         self.graphWindowsNum = 0
+        self.figureNum = 0
         self.buildCellVolt = None
         self.buildCurrent = None
         self.buildVoltage = None
@@ -36,19 +28,27 @@ class LogGraph:
         self.buildCuvThreshold = None
         self.buildChargeVoltageThreshold = None
         self.buildDischargeVoltageThreshold = None
-        self.figureNum = 0
+        self.DATA_LIST = []
+        self.SIGNAL_LIST = []
 
     def ParseFile(self, fileContent):
         self.samplesNum = len(fileContent) - DATA_START_IN_LOGFILE
-        DATA_LIST.clear()
+        self.DATA_LIST.clear()
+        self.SIGNAL_LIST.clear()
+        for signalName in (fileContent[SIGNAL_LINE_IN_LOGFILE].split(',')):
+            self.SIGNAL_LIST.append(signalName)
         for signalNum in range(SIGNALS_NUMBER):
-            DATA_LIST.append([])
+            self.DATA_LIST.append([])
             for dataNum in range(self.samplesNum):
-                DATA_LIST[signalNum].append(fileContent[dataNum + DATA_START_IN_LOGFILE].split(',')[signalNum])
+                self.DATA_LIST[signalNum].append(fileContent[dataNum + DATA_START_IN_LOGFILE].split(',')[signalNum])
+
+    def CalculateXaxis(self):
+        timeList = list([data.split(' ')[1] for data in self.DATA_LIST[self.SIGNAL_LIST.index(XAXIS_RAW_IN_LOGFILE)]])
+        xAxisList = [str(time.datetime.strptime(timeStep, "%H:%M:%S") - time.datetime.strptime(timeList[0], "%H:%M:%S")) for timeStep in timeList[0:self.samplesNum]]
+        return xAxisList
 
     def DrawThreshold(self, *thresholds):
-        timeList = list([data.split(' ')[1] for data in DATA_LIST[SIGNAL_LIST.index("DateTime")]])
-        xAxis = [str(time.datetime.strptime(timeStep, "%H:%M:%S") - time.datetime.strptime(timeList[0], "%H:%M:%S")) for timeStep in timeList[0:self.samplesNum]]
+        xAxis = self.CalculateXaxis()
         plot.figure(self.figureNum)
         for threshold in thresholds:
             plot.plot(xAxis, [threshold]*self.samplesNum, "--")
@@ -56,8 +56,7 @@ class LogGraph:
     def DrawSignal(self, *signals):
         self.figureNum = self.figureNum + 1
         # Calculating X axis time stamps
-        timeList = list([data.split(' ')[1] for data in DATA_LIST[SIGNAL_LIST.index("DateTime")]])
-        xAxis = [str(time.datetime.strptime(timeStep, "%H:%M:%S")-time.datetime.strptime(timeList[0], "%H:%M:%S")) for timeStep in timeList[0:self.samplesNum]]
+        xAxis = self.CalculateXaxis()
         # Creating figure with "figureNum" id and adding a title and icon
         plot.figure(self.figureNum).canvas.set_window_title(f"Лог №{self.graphWindowsNum}")
         thisManager = plot.get_current_fig_manager()
@@ -65,11 +64,11 @@ class LogGraph:
         # Drawing graphs
         __allData = []
         for signalName in signals:
-            signal = SIGNAL_LIST.index(signalName)
-            DATA_LIST[signal] = [int(data) for data in DATA_LIST[signal]]
-            for eachData in DATA_LIST[signal]:
+            signal = self.SIGNAL_LIST.index(signalName)
+            self.DATA_LIST[signal] = [int(data) for data in self.DATA_LIST[signal]]
+            for eachData in self.DATA_LIST[signal]:
                 __allData.append(eachData)
-            plot.plot(xAxis, DATA_LIST[signal], label=signalName)
+            plot.plot(xAxis, self.DATA_LIST[signal], label=signalName)
             plot.legend()
         plot.gca().xaxis.set_major_locator(tck.MultipleLocator(base=ceil(self.samplesNum / 5)))
         plot.xticks(rotation=45)
@@ -87,7 +86,7 @@ class LogGraph:
         }
         # Building voltages graph if related checkbox is checked
         if self.buildCellVolt.get() == True:
-            voltageData = SIGNAL_LIST[SIGNAL_LIST.index("CellVolt1"):SIGNAL_LIST.index("CellVolt1")+int(self.inputCellsNum.get())]
+            voltageData = self.SIGNAL_LIST[self.SIGNAL_LIST.index("CellVolt1"):self.SIGNAL_LIST.index("CellVolt1")+int(self.inputCellsNum.get())]
             self.DrawSignal(*voltageData)
             # Building COV threshold if related checkbox is checked
             if self.buildCovThreshold.get():
